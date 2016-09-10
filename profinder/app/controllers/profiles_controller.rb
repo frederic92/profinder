6eeda0a60b596ef7a60699089037235da0990719
Frederic <fredericmbea@gmail.com>
2016-09-10 20:14:11 +0000

    Added partial form to edit page

--- profinder/app/controllers/profiles_controller.rb
+++ profinder/app/controllers/profiles_controller.rb
@@ -17,12 +17,9 @@ class ProfilesController < ApplicationController
             
     end
     
-    def show
-        
-    end
-    
     def edit
-        
+         @user = User.find( params[:user_id])
+         @profile = @user.profile
     end
     
     def update
